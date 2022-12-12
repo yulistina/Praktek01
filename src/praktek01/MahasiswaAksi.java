b16b84ebcc307c0516b7d70ea9048f186673cd81
yulistina <yulistina101@gmail.com>
2022-12-13 10:28:22 +1400

    Instance Object

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -10,5 +10,8 @@ package praktek01;
  * @author HP 14-an017AU
  */
 public class MahasiswaAksi {
-    
+    public static void main(String[] args) {
+        Mahasiswa Yulis = new Mahasiswa();
+        
+    }
 }
